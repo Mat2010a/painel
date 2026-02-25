b219501787f9180d4757620b2eaa6d74fa8bf7db
Mat2010a <soumeninobomdaselecao@gmail.com>
2026-02-25 17:30:41 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// ⚠️ Este ficheiro contém a chave secreta da API.
+// NÃO enviar para o GitHub — está no .gitignore
+
+const GROQ_API_KEY = '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';
